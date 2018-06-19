8295ef5f6bcb38cb39f906b46d0e0dfc52db75f1
vagart <vagart.77a@mail.ru>
2018-06-19 15:34:52 +0300

    Created TableLayout and DraiverLayout

---
+++ app/src/main/java/com/example/natali/remindme/fragment/ExampleFragment.java
@@ -0,0 +1,32 @@
+package com.example.natali.remindme.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.natali.remindme.R;
+
+public class ExampleFragment extends Fragment {
+    private static final int LAYOUT = R.layout.fragment_example;
+    private View view;
+
+    public static ExampleFragment getInstance(){
+        Bundle args = new Bundle();
+        ExampleFragment fragment = new ExampleFragment();
+        //args.putInt("Напоминания",1);
+        fragment.setArguments(args);
+
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(LAYOUT, container, false);
+        return view;
+    }
+}
